71a2d61d03865a5a8ef55ab330a0f77cfa48feb0
Cynthia Ortiz <cynortiz21@gmail.com>
2024-04-09 23:37:02 -0300

    separamos las card

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import "bootstrap-icons/font/bootstrap-icons.min.css";
 import React from "react";
 import Inicio from "./components/common/pages/Inicio";
 import Error404 from "./components/./common/pages/Error404";
+import AcercaDe from "./components/common/pages/AcercaDe";
 
 function App() {
   return (
     <div>
     {/* <Inicio></Inicio> */}
-    <Error404></Error404>
-
+    {/* <Error404></Error404> */}
+    <AcercaDe></AcercaDe>
     </div>
   );
 }
