af67b5508adf37848995358d9eb5cc3e7e451964
Cynthia Ortiz <cynortiz21@gmail.com>
2024-04-13 21:38:00 -0300

    agregue el carrousel, comprimi las imagenes del mismo y agregue una pequeña descripcion de la app

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -2,7 +2,7 @@ import '../../../styles/menu.css'
 const Footer = () => {
     return (
         <footer className="text-center footer text-light py-4">
-            <p className="text-black">&copy; Todos los derechos reservados Lisandro Civili</p>
+            <p className="text-black">&copy; Todos los derechos reservados</p>
         </footer>
     );
 };
--- src/components/pages/Inicio.jsx
+++ src/components/pages/Inicio.jsx
@@ -1,8 +1,11 @@
-import { Container, Row } from "react-bootstrap";
+import { Container, Row, Carousel } from "react-bootstrap";
 import CardProducto from "./productos/CardProducto";
 import { useEffect, useState } from "react";
-import banner from "../../assets/banner.webp";
-import bannerSmall from "../../assets/bannerSmall.png";
+import banner from "../../assets/bannerFastFood.png";
+import bannerPP from "../../assets/bannerPP-min.jpg";
+import bannerFF from "../../assets/bannerFF-min.jpg";
+import bannerPapitas from "../../assets/banner_papitas-min.jpg";
+import DetalleProducto from "./productos/DetalleProducto";
 import "../../../styles/Inicio.css";
 import { leerProductos } from "../../helpers/queries";
 
@@ -24,26 +27,48 @@ const Inicio = () => {
     }
   };
 
-
   const agregarAlCarrito = (producto) => {
-   
     let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
-
     carrito.push(producto);
     localStorage.setItem("carrito", JSON.stringify(carrito));
   };
 
   return (
     <section className="mainSection">
-      <div className="contBanner">
-        {" "}
-        <img className="banner" src={banner} alt="fondo comida" />
-      </div>
-      <div className="contBannerSmall">
-        {" "}
-        <img className="bannerSmall" src={bannerSmall} alt="fondo comida" />
-      </div>
+      <Carousel>
+        <Carousel.Item className="first-slide">
+          <img
+            className="d-block w-100 img-fluid"
+            src={banner}
+            alt="First slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-100" src={bannerPP} alt="Second slide" />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-100" src={bannerFF} alt="Third slide" />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={bannerPapitas}
+            alt="Forty slide"
+          />
+        </Carousel.Item>
+      </Carousel>
+
       <Container className="mt-5 text-center">
+        <div className="intro">
+          <h1>BIENVENIDO A FLASH FOOD</h1>
+          <p>
+            ¿Hambre repentina? ¿Sin ganas de cocinar? ¡Descarga Flash Food y
+            disfruta de la comida rápida que te encanta en solo unos clics!
+            Olvídate de las filas y la espera. Con Flash Food, pides desde la
+            comodidad de tu casa u oficina y recibes tu pedido ¡calentito y
+            delicioso!
+          </p>
+        </div>
         <h1 className="display-4">Nuestros Productos</h1>
         <hr />
         <Row>
@@ -51,7 +76,7 @@ const Inicio = () => {
             <CardProducto
               key={producto.id}
               producto={producto}
-              agregarAlCarrito={agregarAlCarrito} 
+              agregarAlCarrito={agregarAlCarrito}
             />
           ))}
         </Row>
@@ -61,4 +86,3 @@ const Inicio = () => {
 };
 
 export default Inicio;
-
