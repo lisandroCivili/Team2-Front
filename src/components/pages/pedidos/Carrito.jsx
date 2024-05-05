f2e3451a8e66f2cad45601a13a0f4c0eba7f76f3
Lisandro Civili <lisandrocivili13@gmail.com>
2024-05-05 15:07:44 -0300

    agrego funcion para que al sumar cantidad de productos se actualice el precio individual de cada item

--- src/components/pages/pedidos/Carrito.jsx
+++ src/components/pages/pedidos/Carrito.jsx
@@ -1,18 +1,17 @@
-import '../../../../styles/carrito.css'
-import "../../../../styles/App.css"
-import React, { useEffect, useState } from 'react';
-import ItemCarrito from './ItemCarrito';
-import Swal from 'sweetalert2';
-import { crearPedido } from '../../../helpers/queries';
-import { Button } from 'bootstrap';
-import { Link } from 'react-router-dom';
-
+import "../../../../styles/carrito.css";
+import "../../../../styles/App.css";
+import React, { useEffect, useState } from "react";
+import ItemCarrito from "./ItemCarrito";
+import Swal from "sweetalert2";
+import carritoVacio from "../../../assets/carrito-vacio.webp";
+import { Button } from "react-bootstrap";
+import { Link, NavLink } from "react-router-dom";
 
 const Carrito = () => {
   const [productosEnCarrito, setProductosEnCarrito] = useState([]);
-
+  const [cantProductos, setcantProductos] = useState(1);
+  const carritoDeStorage = JSON.parse(sessionStorage.getItem("carrito")) || [];
   useEffect(() => {
-    const carritoDeStorage = JSON.parse(sessionStorage.getItem("carrito")) || [];
     setProductosEnCarrito(carritoDeStorage);
   }, []);
 
@@ -24,41 +23,57 @@ const Carrito = () => {
       denyButtonText: `Cancelar`,
     }).then((result) => {
       if (result.isConfirmed) {
-        const carritoActualizado = productosEnCarrito.filter(producto => producto._id !== id);
+        const carritoActualizado = productosEnCarrito.filter(
+          (producto) => producto._id !== id
+        );
         sessionStorage.setItem("carrito", JSON.stringify(carritoActualizado));
-        
+
         setProductosEnCarrito(carritoActualizado);
-    }
-  });
-}
-    
+      }
+    });
+  };
 
-    return (
-        <section className="pt-5 pb-5">
-            <div className="container">
-                <div className="row w-100">
-                    <div className="col-lg-12 col-md-12 col-12" >
-                        <table id="table1" className=" table  table-responsive " >
-                            <thead>
-                                <tr>
-                                    <th id='th1'>Producto</th>
-                                    <th id='th2'>Precio</th>
-                                    <th id='th3'>Cantidad</th>
-                                    <th id='th4'></th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {
-                                    productosEnCarrito.map(producto => <ItemCarrito key={producto._id} producto={producto} eliminarDelCarrito={eliminarDelCarrito}/>)
-                                }
-                            </tbody>
-                        </table>
-                      
-                    </div>
-                </div>
-            </div>
-        </section>
-    );
-}
+  return (
+    <section className="pt-5 pb-5">
+      <div className="container">
+        <div className="row w-100">
+          <div className="col-lg-12 col-md-12 col-12">
+            {carritoDeStorage.length === 0 ? (
+              <div className="carritoVacio">
+                <img src={carritoVacio} alt="" />
+                <p>Todavia no tiene ningun pedido!</p>
+                <NavLink className="volverAInicio" to="/">
+                  Ir a agregar productos
+                </NavLink>
+              </div>
+            ) : (
+              <table id="table1" className=" table  table-responsive ">
+                <thead>
+                  <tr>
+                    <th id="th1">Producto</th>
+                    <th id="th2">Precio</th>
+                    <th id="th3">Cantidad</th>
+                    <th id="th4"></th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {productosEnCarrito.map((producto) => (
+                    <ItemCarrito
+                      key={producto._id}
+                      producto={producto}
+                      eliminarDelCarrito={eliminarDelCarrito}
+                      setcantProductos={setcantProductos}
+                      cantProductos={cantProductos}
+                    />
+                  ))}
+                </tbody>
+              </table>
+            )}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Carrito ;
+export default Carrito;
