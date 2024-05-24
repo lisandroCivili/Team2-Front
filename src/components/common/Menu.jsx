b9c70fcc165415bbbd1a87e62af2461b677d1986
Lisandro Civili <lisandrocivili13@gmail.com>
2024-05-24 00:00:36 -0300

    divido los botones del nav para md y l

--- src/components/common/Menu.jsx
+++ src/components/common/Menu.jsx
@@ -7,27 +7,26 @@ import { useEffect, useState } from "react";
 const Menu = () => {
   const navegacion = useNavigate();
   const [estaLogeado, setEstaLogeado] = useState(false);
-  
+
   const usuario = JSON.parse(sessionStorage.getItem("usuario")) || "";
-  let usuarioCorrecto = true
+  let usuarioCorrecto = true;
   if (!usuario) {
-    usuarioCorrecto = false
+    usuarioCorrecto = false;
   }
 
   useEffect(() => {
-    setEstaLogeado(usuario)
+    setEstaLogeado(usuario);
   }, [usuarioCorrecto]);
 
-  
   const logout = () => {
     sessionStorage.removeItem("usuario");
-    sessionStorage.removeItem("carrito")
+    sessionStorage.removeItem("carrito");
     setUsuarioLogeado(false);
     navegacion("/login");
   };
   return (
     <Navbar expand="md" className="navbar">
-      <Container>
+      <Container className="contBotones">
         <Navbar.Brand as={Link} to="/" href="#home">
           <img
             src={logo}
@@ -36,11 +35,24 @@ const Menu = () => {
           />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto" id="nav">
-            <NavLink className="nav-link" id="inicioBoton" to="/">
-              INICIO
-            </NavLink>
+        <Navbar.Collapse id="basic-navbar-nav" className="navCollapseBotones">
+          <Nav className="me-auto navContBotones" id="nav">
+            <div className="botonesIzq">
+              <NavLink className="nav-link" id="inicioBoton" to="/">
+              <i class="bi bi-house-door-fill iconoInicio"></i>
+              </NavLink>
+              {estaLogeado && estaLogeado.rol === "Admin" ? (
+                <NavLink
+                  className="nav-link"
+                  id="adminBoton"
+                  to="/administrador"
+                >
+                  Administración
+                </NavLink>
+              ) : (
+                <></>
+              )}
+            </div>  
             {!estaLogeado ? (
               <>
                 <NavLink className="nav-link" id="loginBoton" to="/login">
@@ -51,21 +63,14 @@ const Menu = () => {
                 </NavLink>
               </>
             ) : (
-              <>
-                <NavLink className="nav-link" id="logoutBoton" onClick={logout}>
-                  LOGOUT
-                </NavLink>
+              <div className="botonesDer">
                 <NavLink className="nav-link" id="carritoBoton" to="/carrito">
-                  <i className="bi bi-cart-fill"></i>
+                  <i className="bi bi-cart-fill iconoCarrito"></i>
                 </NavLink>
-              </>
-            )}
-            {estaLogeado && estaLogeado.rol === "Admin" ? (
-              <NavLink className="nav-link" id="adminBoton" to="/administrador">
-              ADMINISTRADOR
-            </NavLink>
-            ) : (
-              <></>
+                <NavLink className="nav-link" id="logoutBoton" onClick={logout}>
+                  <i alt="Cerrar Sesion" className="bi bi-box-arrow-right iconoCerrarSesion"></i>
+                </NavLink>
+              </div>
             )}
           </Nav>
         </Navbar.Collapse>
